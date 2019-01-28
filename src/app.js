af3adb21202be9f76114c5ee6226b3b8139c7179
feng <fengjc_sleep@163.com>
2019-01-28 10:32:46 +0800

    添加chai和chai-spies

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
+
+var expect = chai.expect;
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -11,4 +16,6 @@ new Vue({
         loading1: true
     }
 })
+
+
 
